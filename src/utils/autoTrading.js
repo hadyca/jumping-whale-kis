29347450952b8e7fbbc37da4f5823d9170e0baf2
unknown <hadycas@gmail.com>
2024-03-12 19:53:43 +0600

    같은구간중복매수방지

--- src/utils/autoTrading.js
+++ src/utils/autoTrading.js
@@ -23,13 +23,14 @@ export default async function autoTrading(token) {
 
   // 자동봇 거래시간
   const nowKoreaHour = getKoreaHour();
-  if (
-    nowKoreaHour < POSITION_FIRST_ENTRY_TIME ||
-    nowKoreaHour > FORCED_LIQUIDATE_TIME
-  ) {
-    console.log("자동 봇 매매 시간이 아닙니다.");
-    return;
-  }
+
+  // if (
+  //   nowKoreaHour < POSITION_FIRST_ENTRY_TIME ||
+  //   nowKoreaHour > FORCED_LIQUIDATE_TIME
+  // ) {
+  //   console.log("자동 봇 매매 시간이 아닙니다.");
+  //   return;
+  // }
 
   //to-be최종거래일일 때 자동거래시간 세팅필요
 
@@ -75,14 +76,25 @@ export default async function autoTrading(token) {
   // );
   // console.log(qty);
 
+  //수익 퍼센티지 설정
   const PROFIT_PERCENT = 0.001; //0.1%
   const LOSS_PERCENT = 0.001;
+
+  //같은 시간 대 중복 매매 되지 않도록 세팅
+  const currentCandleTime = candleValue[0].stck_cntg_hour;
+  const haveTimeBuyPosition = BuyPositionAry.some(
+    (obj) => obj.tradingCandleTime === currentCandleTime
+  );
+  const haveTimeSellPosition = SellPositionAry.some(
+    (obj) => obj.tradingCandleTime === currentCandleTime
+  );
 
   //시장가 매수 포지션 진입
   if (
     rsiData.beforeRsi < SET_ROW_RSI &&
     rsiData.nowRsi > SET_ROW_RSI &&
-    nowKoreaHour < POSITION_LAST_ENTRY_TIME
+    nowKoreaHour < POSITION_LAST_ENTRY_TIME &&
+    !haveTimeBuyPosition
   ) {
     const marketBuyResult = await marketOrder(
       token,
@@ -99,6 +111,7 @@ export default async function autoTrading(token) {
     const newPosition = {
       id: "123",
       side: "buy",
+      tradingCandleTime: candleValue[0].stck_cntg_hour, //매매 시간 구간
       unit: "1",
       price: candleValue[0].futs_prpr, //임시로 현재가 넣었고, 추 후 실제 매매 데이터 확인해서 넣어야함
       totalPrice: "1" * candleValue[0].futs_prpr,
@@ -107,7 +120,6 @@ export default async function autoTrading(token) {
       lossPrice:
         candleValue[0].futs_prpr - candleValue[0].futs_prpr * PROFIT_PERCENT,
     };
-
     BuyPositionAry.push(newPosition);
   }
 
@@ -115,7 +127,8 @@ export default async function autoTrading(token) {
   if (
     rsiData.beforeRsi > SET_HIGH_RSI &&
     rsiData.nowRsi < SET_HIGH_RSI &&
-    nowKoreaHour < POSITION_LAST_ENTRY_TIME
+    nowKoreaHour < POSITION_LAST_ENTRY_TIME &&
+    !haveTimeSellPosition
   ) {
     const marketSellResult = await marketOrder(
       token,
@@ -131,6 +144,7 @@ export default async function autoTrading(token) {
     const newPosition = {
       id: "123",
       side: "sell",
+      tradingCandleTime: candleValue[0].stck_cntg_hour, //매매 시간 구간
       unit: "1",
       price: candleValue[0].futs_prpr,
       totalPrice: "1" * candleValue[0].futs_prpr,
@@ -142,13 +156,15 @@ export default async function autoTrading(token) {
     SellPositionAry.push(newPosition);
   }
 
-  //to-be:1) 익절/손절 구간 시 청산 로직  2)익절/손절 구간이 아니지만 장 종료전 청산 잔량이 남았을 때 청산 로직
+  //to-be:익절/손절 구간 시 청산 로직  (find함수 써야할듯?)
+
+  //to-be:익절/손절 구간이 아니지만 장 종료전 청산 잔량이 남았을 때 청산 로직(현재시간이 설정한 강제청산시간과 같을 때, 포지션 배열에 값이 남아있다면 청산 진행)
 
   console.log(rsiData);
   console.log("현재가:", candleValue[0].futs_prpr);
   console.log(
     "보유 매수 포지션:",
-    BuyPositionAryPositionAry,
+    BuyPositionAry,
     "/ 보유 매도 포지션:",
     SellPositionAry
   );
